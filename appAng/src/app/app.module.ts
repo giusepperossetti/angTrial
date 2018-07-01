e7da5acfce30731957e532cb13326573383a96dc
giusepperossetti <peppeross995@gmail.com>
2018-07-01 20:34:24 +0200

    Inserita navbar con bootstrap.

--- appAng/src/app/app.module.ts
+++ appAng/src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http'; 
+ 
 import { AppComponent } from './app.component';
+import { NgbdCarouselBasic } from './carousel/carousel.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NgbdCarouselBasic,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpClientModule,
     NgbModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent, NgbdCarouselBasic]
 })
 export class AppModule { }
