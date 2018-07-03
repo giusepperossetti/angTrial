9fe98d27767c4272b1cb72563b1324e90c31fb1e
unknown <peppeross995@gmail.com>
2018-07-03 17:31:39 +0200

    delete all fortawesome modules and use only angular-font-awesome

--- appAng/src/app/app.module.ts
+++ appAng/src/app/app.module.ts
@@ -2,15 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { AngularFontAwesomeModule } from 'angular-font-awesome'; // Imports angular-font-awesome 
 
 
 import { AppComponent } from './app.component';
 import { NgbdCarouselBasic } from './carousel/carousel.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NgbdDropdownBasic } from './dropdown/dropdown.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { FacebookIconComponent } from './facebook-icon/facebook-icon.component';
 
 
 @NgModule({
@@ -18,16 +17,16 @@ import { FacebookIconComponent } from './facebook-icon/facebook-icon.component';
     AppComponent,
     NgbdCarouselBasic, // Slide Template Component
     NavbarComponent, // Navbar
-    NgbdDropdownBasic, FacebookIconComponent // Dropdown menu in navbar
+    NgbdDropdownBasic // Dropdown menu in navbar
   ],
   imports: [
     BrowserModule,
-    FontAwesomeModule, // Font Awesome
     FormsModule,
     HttpClientModule,
+    AngularFontAwesomeModule,
     NgbModule.forRoot()
   ],
   providers: [],
-  bootstrap: [AppComponent, NgbdCarouselBasic]
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
